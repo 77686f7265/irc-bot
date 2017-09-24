f591b0db001b1b516509e4173aec1225e99ab4c5
kizuthemaid <rbshayla@yahoo.co.jp>
2017-09-24 18:46:10 +0200

    v0.1.0

--- index.js
+++ index.js
@@ -4,10 +4,23 @@ var client = require('coffea')({
   channels: ['#sakurasou']
 });
 
+
+
 client.on('command', function (event) {
   switch (event.cmd) {
     case 'ping':
       event.reply('pong');
       break;
-  }
+    
+    case 'restart':
+    process.exit();
+    break;
+   }
+});
+
+client.on('message', function(message){
+	var date = new Date();
+	var formattedDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+	console.log(formattedDate + " : "+ message.message);
 });
